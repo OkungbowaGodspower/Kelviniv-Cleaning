c8bbc8de246bdd35410595d5a943f9711c7cefa5
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2025-02-16 00:30:08 +0100

    Add script file

---
+++ Kelviniv/script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+
+});
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
